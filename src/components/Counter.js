5331f379dce3bd5a0f792e8801e57ee5d9b8b7da
Alex Novitsky <novitsky.413x@gmail.com>
2024-03-19 14:32:57 +0300

    replace redux reducer with rtk slice, start work with auth

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,43 @@
 import { useSelector, useDispatch } from 'react-redux';
-
+import { counterActions } from '../store';
 import classes from './Counter.module.css';
+
+// const Counter = () => {
+//     const dispatch = useDispatch();
+//     const counter = useSelector((state) => state.counter);
+//     const show = useSelector((state) => state.showCounter);
+
+//     const incrementHandler = () => {
+//         dispatch({ type: 'increment' });
+//     };
+
+//     const increaseHandler = () => {
+//         dispatch({ type: 'increase', amount: 5 });
+//     };
+
+//     const decrementHandler = () => {
+//         dispatch({ type: 'decrement' });
+//     };
+
+//     const toggleCounterHandler = () => {
+//         dispatch({ type: 'toggle' });
+//     };
+
+//     return (
+//         <main className={classes.counter}>
+//             <h1>Redux Counter</h1>
+//             {show && <div className={classes.value}>{counter}</div>}
+//             <div>
+//                 <button onClick={decrementHandler}>Decrement</button>
+//                 <button onClick={increaseHandler}>Increase by 5</button>
+//                 <button onClick={incrementHandler}>Increment</button>
+//             </div>
+//             <button onClick={toggleCounterHandler}>Toggle Counter</button>
+//         </main>
+//     );
+// };
+
+// export default Counter;
 
 const Counter = () => {
     const dispatch = useDispatch();
@@ -8,19 +45,19 @@ const Counter = () => {
     const show = useSelector((state) => state.showCounter);
 
     const incrementHandler = () => {
-        dispatch({ type: 'increment' });
+        dispatch(counterActions.increment());
     };
 
     const increaseHandler = () => {
-        dispatch({ type: 'increase', amount: 5 });
+        dispatch(counterActions.increase(5));
     };
 
     const decrementHandler = () => {
-        dispatch({ type: 'decrement' });
+        dispatch(counterActions.decrement());
     };
 
     const toggleCounterHandler = () => {
-        dispatch({ type: 'toggle' });
+        dispatch(counterActions.toggleCounter());
     };
 
     return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,71 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
     counter: 0,
     showCounter: true,
 };
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        };
-    }
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        };
-    }
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter,
-        };
-    }
-    return state;
-};
 
-const store = createStore(counterReducer);
+// Sice of global state. Every slice needs a name
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        // (state, action) - no need to pass actions, it will be tracked automatically
+        increment(state) {
+            // state CAN BE MUTATED
+            // Immer.js automatically clones state object to prevent mutations
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter += action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
+    },
+});
+// // Old way of handling actions.
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//     if (action.type === 'toggle') {
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter,
+//         };
+//     }
+//     return state;
+// };
+// const store = createStore(counterReducer);
 
+// // To see automatically generated action tags
+// console.log(counterSlice.actions.toggleCounter)
+
+const store = configureStore({
+    //  // A "map" of reducers
+    //  reducer: { counter: counterSlice.reducer },
+    // A single reducer
+    reducer: counterSlice.reducer,
+});
+export const counterActions = counterSlice.actions;
 export default store;
